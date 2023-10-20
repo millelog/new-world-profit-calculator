b7269ab09a450cd2b04f729fc939b549a9149b99
Logan Miller <millerl1960@gmail.com>
2023-10-20 14:19:20 -0700

    Crafting profit refactor

--- analysis/crafting_profit.py
+++ analysis/crafting_profit.py
@@ -6,189 +6,203 @@ import database.operations.recipe_operations as ro
 import database.operations.player_operations as po
 import database.operations.item_operations as io
 
-MAX_DEPTH=10
-
-def calculate_item_cost(session, item_id, server_id, player_id, depth=0):
-    """
-    Calculates the cost of an item either by crafting or market price.
-    """
-    if depth > MAX_DEPTH:
-        # Return market price or some default value
-        market_price_data = cpo.get_current_price(session, item_id, server_id)
-        market_price = market_price_data['price'] if market_price_data else float('inf')
-        return market_price, {}
-
-    # Fetch potential crafting recipes for the item
-    recipes = ro.get_recipes_by_item(session, item_id)
-
-    # Fetch the market price for the item
-    market_price_data = cpo.get_current_price(session, item_id, server_id)
-    market_price = market_price_data['price'] if market_price_data else float('inf')
-
-    # If no crafting recipes, default to market price
-    if not recipes:
-        return market_price, {}
-
-    # If recipes exist, determine the cheapest method
-    crafting_options = [
-        get_crafting_cost(session, recipe, server_id, player_id, depth)
-        for recipe in recipes
-    ]
-    min_crafting_cost = min(crafting_options, key=lambda x: x[0])[0]
-
-    # Compare market price with crafting cost and return the cheaper option
-    if market_price <= min_crafting_cost:
-        return market_price, {}
-    else:
-        return min(crafting_options, key=lambda x: x[0])
+class CraftingProfitAnalyzer:
+    def __init__(self, session, server_id, player_id):
+        self.session = session
+        self.server_id = server_id
+        self.player_id = player_id
 
 
+    def calculate_item_cost(self, item_id, market_price):
+        """
+        Calculates the cost of an item either by crafting or market price.
+        Compares crafting cost with market price and stops crafting computation 
+        if the cost exceeds market price.
+        """
+        # Fetch potential crafting recipes for the item
+        recipes = ro.get_recipes_by_item(self.session, item_id)
+
+        # If no crafting recipes are found, return the market price
+        if not recipes:
+            return market_price, {}
+
+        # For each crafting recipe, determine the cost and compare it to the market price
+        cheapest_crafting_cost = float('inf')
+        cheapest_crafting_tree = {}
+        for recipe in recipes:
+            crafting_cost, crafting_tree = self.get_crafting_cost(recipe, market_price)
+
+            # If the crafting cost for this recipe is cheaper, update the cheapest cost and tree
+            if crafting_cost < cheapest_crafting_cost:
+                cheapest_crafting_cost = crafting_cost
+                cheapest_crafting_tree = crafting_tree
+
+            # If at any point the crafting cost surpasses the market price, return market price
+            if cheapest_crafting_cost > market_price:
+                return market_price, {}
+            
+
+        return cheapest_crafting_cost, cheapest_crafting_tree
 
 
-def get_reagent_item_id(session, reagent, used_reagents, server_id):
-    """
-    Determines the reagent item ID for crafting, prioritizing specific item IDs or selecting from available item types.
-    """
-    if reagent.reagent_item_id:
-        return reagent.reagent_item_id
-    potential_items = io.get_items_for_item_type(session, reagent.reagent_item_type_id, server_id) #returns sorted by market price
-    potential_items = [item for item in potential_items if item.item_id not in used_reagents]  # Filter out already used reagents
-    if not potential_items:
-        return None  # No valid items to use for this reagent
-    # Assuming potential_items is sorted by price, return cheapest
-    return potential_items[0].item_id
+    def get_reagent_item_id(self, reagent, used_reagents):
+        """
+        Determines the reagent item ID for crafting, prioritizing specific item IDs or selecting from available item types.
+        """
+        if reagent.reagent_item_id:
+            return reagent.reagent_item_id
+
+        potential_items = io.get_items_for_item_type(self.session, reagent.reagent_item_type_id, self.server_id)  # Returns sorted by market price
+        potential_items = [item for item in potential_items if item.item_id not in used_reagents]  # Filter out already used reagents
+
+        if not potential_items:
+            return None  # No valid items to use for this reagent
+
+        # Assuming potential_items is sorted by price, return the cheapest one
+        return potential_items[0].item_id
 
 
-def get_crafting_cost(session, recipe, server_id, player_id, depth=0):
-    """
-    Calculates the crafting cost of a given recipe.
-    """
-    # Check if the player can craft the given recipe
-    can_craft = po.can_craft_recipe(session, player_id, recipe.recipe_id)
-    if not can_craft:
-        market_price_data = cpo.get_current_price(session, recipe.result_item_id, server_id)
-        market_price = market_price_data['price'] if market_price_data else float('inf')  # Assume infinite cost if no market price data
-        return market_price, {}
+    def get_crafting_cost(self, recipe, market_price, accumulated_cost=0):
+        """
+        Calculates the crafting cost of a given recipe while considering the market price.
+        """
+        # Check if the player can craft the given recipe
+        can_craft = po.can_craft_recipe(self.session, self.player_id, recipe.recipe_id)
+        if not can_craft:
+            result_market_price_data = cpo.get_current_price(self.session, recipe.result_item_id, self.server_id)
+            return result_market_price_data['price'] if result_market_price_data else float('inf'), {}
 
-    total_cost = 0
-    crafting_tree = {}
+        total_cost = accumulated_cost
+        crafting_tree = {}
 
-    local_used_reagents = set()  # For item tyupe uniqueness within this recipe level
-    for reagent in recipe.recipe_reagents:
-        reagent_item_id = get_reagent_item_id(session, reagent, local_used_reagents, server_id)
-        if not reagent_item_id:
-            return float('inf'), {}
+        local_used_reagents = set()  # For item type uniqueness within this recipe level
+        for reagent in recipe.recipe_reagents:
+            reagent_item_id = self.get_reagent_item_id(reagent, local_used_reagents)
+            if not reagent_item_id:
+                return float('inf'), {}
 
-        local_used_reagents.add(reagent_item_id)
+            local_used_reagents.add(reagent_item_id)
 
-        reagent_quantity = reagent.quantity_required
+            reagent_quantity = reagent.quantity_required
 
-        # Calculate cost for the reagent
-        reagent_cost, reagent_tree = calculate_item_cost(session, reagent_item_id, server_id, player_id, depth + 1)
+            # Calculate cost for the reagent
+            reagent_market_price_data = cpo.get_current_price(self.session, reagent_item_id, self.server_id)
+            reagent_market_price = reagent_market_price_data['price'] if reagent_market_price_data else float('inf')
+            reagent_cost, reagent_tree = self.calculate_item_cost(reagent_item_id, reagent_market_price)
+
+
+            # If at any point, the accumulated cost surpasses the market price, return market price
+            total_cost += reagent_cost * reagent_quantity
+            if total_cost > market_price:
+                return market_price, {}
+
+            crafting_tree[reagent_item_id] = {
+                'cost': reagent_cost,
+                'quantity': reagent_quantity,
+                'source': 'crafted' if reagent_tree else 'market',
+                'children': reagent_tree
+            }
+
+        # Adjust for the quantity produced by the recipe
+        unit_cost = total_cost / recipe.quantity_produced if recipe.quantity_produced else total_cost
+
+        # Include any crafting fee (assumed to be 0 for simplicity)
+        unit_cost += 0  # Adjust if there's a crafting fee
         
-        crafting_tree[reagent_item_id] = {
-            'cost': reagent_cost,
-            'quantity': reagent_quantity,
-            'source': 'crafted' if reagent_tree else 'market',
-            'children': reagent_tree
+        return unit_cost, crafting_tree
+
+
+    def _normalize_values(self, values):
+        """
+        Normalize values to a range of 0 to 100 based on their position in the range of all items.
+        """
+        min_val = min(values)
+        max_val = max(values)
+        if max_val == min_val:
+            return [100 if v == max_val else 0 for v in values]
+        else:
+            return [(v - min_val) / (max_val - min_val) * 100 for v in values]
+        
+
+    def calculate_score(self, profitability_info):
+        """
+        Calculate score based on normalized profitability factors.
+        """
+        profits = [info["Profit"] for info in profitability_info.values()]
+        profit_margins = [info["Profit Margin"] for info in profitability_info.values()]
+        availabilities = [info["Availability"] for info in profitability_info.values()]
+
+        # Normalize values
+        normalized_profits = self._normalize_values(profits)
+        normalized_profit_margins = self._normalize_values(profit_margins)
+        normalized_availabilities = self._normalize_values(availabilities)
+
+        # Calculate scores for each item
+        for idx, item_id in enumerate(profitability_info.keys()):
+            score = normalized_profits[idx] * normalized_profit_margins[idx] * normalized_availabilities[idx]
+            profitability_info[item_id]["Score"] = score
+
+        return profitability_info
+
+
+    def calculate_profitability(self, item_id):
+        """
+        Calculate profitability for an item based on crafting or buying.
+        """
+        market_price_data = cpo.get_current_price(self.session, item_id, self.server_id)
+        if not market_price_data:
+            return None
+
+        market_price = market_price_data['price']
+
+        # Calculate crafting cost for the item
+        crafting_cost, crafting_tree = self.calculate_item_cost(item_id, market_price)  # Pass the market_price here
+        profit = market_price - crafting_cost
+        profit_margin = (profit / crafting_cost) * 100 if crafting_cost != 0 else 0
+        availability = market_price_data['availability'] if market_price_data else 0
+
+        return {
+            "Crafting Tree": crafting_tree,
+            "Profit": profit,
+            "Profit Margin": profit_margin,
+            "Crafting Cost": crafting_cost,
+            "Market Price": market_price,
+            "Availability": availability
         }
-        
-        # Accumulate the cost
-        total_cost += reagent_cost * reagent_quantity
-
-    # Adjust for the quantity produced by the recipe
-    unit_cost = total_cost / recipe.quantity_produced if recipe.quantity_produced else total_cost
-
-    # Include any crafting fee (assumed to be 0 for simplicity)
-    unit_cost += 0  # Adjust if there's a crafting fee
-    
-    return unit_cost, crafting_tree
 
 
 
-def normalize_values(values):
-    """
-    Normalize values to a range of 0 to 100 based on their position in the range of all items.
-    """
-    min_val = min(values)
-    max_val = max(values)
-    if max_val == min_val:
-        return [100 if v == max_val else 0 for v in values]
-    else:
-        return [(v - min_val) / (max_val - min_val) * 100 for v in values]
-    
+    def evaluate_all_recipes(self, callback=None):
+        """
+        Evaluate profitability for all recipes in the database.
+        """
+        all_recipes = self.session.query(CraftingRecipe).all()
+        total_recipes = len(all_recipes)
+        profitability_info = {}
 
-def calculate_score(profitability_info):
-    """
-    Calculate score based on normalized profitability factors.
-    """
-    profits = [info["Profit"] for info in profitability_info.values()]
-    profit_margins = [info["Profit Margin"] for info in profitability_info.values()]
-    availabilities = [info["Availability"] for info in profitability_info.values()]
+        for index, recipe in enumerate(all_recipes):
+            item_id = recipe.result_item_id
+            market_price_data = cpo.get_current_price(self.session, item_id, self.server_id)
+            if not market_price_data:
+                continue
 
-    # Normalize values
-    normalized_profits = normalize_values(profits)
-    normalized_profit_margins = normalize_values(profit_margins)
-    normalized_availabilities = normalize_values(availabilities)
+            market_price = market_price_data['price']
+            
+            profitability_data = self.calculate_profitability(item_id)
+            
+            if not profitability_data:
+                continue
 
-    # Calculate scores for each item
-    for idx, item_id in enumerate(profitability_info.keys()):
-        score = normalized_profits[idx] * normalized_profit_margins[idx] * normalized_availabilities[idx]
-        profitability_info[item_id]["Score"] = score
+            profitability_info[item_id] = profitability_data
 
-    return profitability_info
+            # Update the progress bar after processing each recipe
+            if callback:
+                callback(index + 1, total_recipes)
 
-def calculate_profitability(session, item_id, server_id, player_id):
-    """
-    Calculate profitability for an item based on crafting or buying.
-    """
-    market_price_data = cpo.get_current_price(session, item_id, server_id)
-    if not market_price_data:
-        return None
+        # Calculate scores based on normalized values
+        profitability_info = self.calculate_score(profitability_info)
 
-    market_price = market_price_data['price']
-
-    # Calculate crafting cost for the item
-    crafting_cost, crafting_tree = calculate_item_cost(session, item_id, server_id, player_id)
-    profit = market_price - crafting_cost
-    profit_margin = (profit / crafting_cost) * 100 if crafting_cost != 0 else 0
-    availability = market_price_data['availability'] if market_price_data else 0
-
-    return {
-        "Crafting Tree": crafting_tree,
-        "Profit": profit,
-        "Profit Margin": profit_margin,
-        "Crafting Cost": crafting_cost,
-        "Market Price": market_price,
-        "Availability": availability
-    }
-
-def evaluate_all_recipes(session, server_id, player_id, callback=None):
-    """
-    Evaluate profitability for all recipes in the database.
-    """
-    all_recipes = session.query(CraftingRecipe).all()
-    total_recipes = len(all_recipes)
-    profitability_info = {}
-
-    for index, recipe in enumerate(all_recipes):
-        
-    
-        item_id = recipe.result_item_id
-        profitability_data = calculate_profitability(session, item_id, server_id, player_id)
-        if not profitability_data:
-            continue
-
-        profitability_info[item_id] = profitability_data
-
-        # Update the progress bar after processing each recipe
-        if callback:
-            callback(index + 1, total_recipes)
-
-    # Calculate scores based on normalized values
-    profitability_info = calculate_score(profitability_info)
-
-    # Sort items by score and get the top
-    top_items = sorted(profitability_info.items(), key=lambda x: x[1]["Score"], reverse=True)[:100]
-    return top_items
+        # Sort items by score and get the top
+        top_items = sorted(profitability_info.items(), key=lambda x: x[1]["Score"], reverse=True)[:100]
+        return top_items
 
--- ui/analysis_frame.py
+++ ui/analysis_frame.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from analysis.crafting_profit import calculate_profitability, evaluate_all_recipes
+from analysis.crafting_profit import CraftingProfitAnalyzer
 from database.operations.item_operations import get_item_by_id
 from ui.graph_frame import ItemGraphFrame
 
@@ -10,25 +10,27 @@ class AnalysisFrame(tk.Frame):
         super().__init__(parent)
         self.session = session
         self.data_store = data_store
+        self.crafting_profit_analyzer = CraftingProfitAnalyzer(self.session, self.data_store.server_id, self.data_store.player_id)
         
         self.progress = ttk.Progressbar(self, orient="horizontal", length=200, mode="determinate")
-        self.progress.grid(row=1, columnspan=2, pady=(5,5))
+        self.progress.grid(row=0, column=1, pady=(5,5))
 
         # Button to evaluate all recipes and find the most profitable
         self.evaluate_button = tk.Button(self, text="Evaluate All Recipes", command=self.evaluate_all_recipes_ui)
-        self.evaluate_button.grid(row=0, columnspan=2)
+        self.evaluate_button.grid(row=0, column=0)
+
         
         # Text widget to display results
         self.result_text = tk.Text(self, width=40, height=10)
-        self.result_text.grid(row=2, columnspan=2)
+        self.result_text.grid(row=1, columnspan=2)
 
          # Create Panedwindow to hold the listbox and treeview
         self.panedwindow = ttk.Panedwindow(self, orient=tk.HORIZONTAL)
-        self.panedwindow.grid(row=3, column=0, columnspan=2, sticky='nsew')
+        self.panedwindow.grid(row=2, column=0, columnspan=2, sticky='nsew')
         
          # Create the ItemPriceGraphFrame and place it in the AnalysisFrame
         self.price_graph_frame = ItemGraphFrame(self, self.session, self.data_store)
-        self.price_graph_frame.grid(row=4, column=0, columnspan=2, sticky='nsew')
+        self.price_graph_frame.grid(row=3, column=0, columnspan=2, sticky='nsew')
 
         # Frame for Listbox
         self.listbox_frame = tk.Frame(self.panedwindow)
@@ -66,7 +68,7 @@ class AnalysisFrame(tk.Frame):
         self.populate_tree(info.get("Crafting Tree", {}))
     
     def evaluate_all_recipes_ui(self):
-        profitability_info_list = evaluate_all_recipes(self.session, self.data_store.server_id, self.data_store.player_id, callback=self.update_progress)
+        profitability_info_list = self.crafting_profit_analyzer.evaluate_all_recipes(callback=self.update_progress)
         # Convert list of tuples to a dictionary
         self.profitability_info = {item_id: info for item_id, info in profitability_info_list}
         self.listbox.delete(0, tk.END)  # Clear existing listbox items
